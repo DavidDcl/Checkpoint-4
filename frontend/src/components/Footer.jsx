65f456c9b96ed01a17bb19061d814b01385c1b48
Suprium <davidlopes0208@gmail.com>
2023-09-27 11:42:08 +0200

    Setup homePage also

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer footer-center p-4 bg-stone-950 text-base-content fixed">
+      <aside>
+        <p>Copyright © 2023 - All right reserved by Dinex Industries Ltd</p>
+      </aside>
+    </footer>
+  );
+}
+
+export default Footer;
